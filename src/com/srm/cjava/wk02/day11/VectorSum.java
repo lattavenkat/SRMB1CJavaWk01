48bab4221a7db13d23dd546a038e975fe6aac74e
Latha <lattanaidu@gmail.com>
2021-07-03 16:18:26 +0530

    cleared cleared

--- src/com/srm/cjava/wk02/day11/VectorSum.java
+++ src/com/srm/cjava/wk02/day11/VectorSum.java
@@ -2,12 +2,6 @@ package com.srm.cjava.wk02.day11;
 
 import java.util.Vector;
 
-// class Addition {
-// public static int calsum(Vector<Integer> value1, Vector<Integer> value2){
-
-// }
-// }
-
 public class VectorSum {
 
     public static void main(String[] args) {
@@ -31,7 +25,7 @@ public class VectorSum {
         // }
         Vector<Integer> v1 = new Vector<>();
         Vector<Integer> v2 = new Vector<>();
-        int sum = 0;
+        
         v1.add(5);
         v1.add(15);
         v1.add(20);
@@ -39,6 +33,9 @@ public class VectorSum {
         v2.add(5);
         v2.add(5);
         v2.add(10);
+        System.out.println("Vector 1: "+v1);
+        System.out.println("Vector 1: "+v2);
+        System.out.println("After Adding two vectors: ");
         Vector<Integer> v3 = new Vector<>();
         for (int i = 0; i < v1.size(); i++) {
             int a = v1.get(i) + v2.get(i);
