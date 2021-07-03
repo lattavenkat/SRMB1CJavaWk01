e05389e1a79714accf96b64e0d1a69ce0e901246
Latha <lattanaidu@gmail.com>
2021-07-03 16:07:39 +0530

    commit

--- src/com/srm/cjava/wk02/day11/BinarySearch.java
+++ src/com/srm/cjava/wk02/day11/BinarySearch.java
@@ -1,8 +1,10 @@
 package com.srm.cjava.wk02.day11;
 
+import java.util.Arrays;
+
 public class BinarySearch {
-    
-    
+
+ 
     // search
     int binarySearch(int array[], int x, int low, int high) {
 
@@ -20,18 +22,25 @@ public class BinarySearch {
         }
 
         return -1;
+
+        
     }
 
     public static void main(String[] args) {
         BinarySearch ob = new BinarySearch();
-        int array[] = { 7, 8, 5, 9, 3, 4, 96};
+        int array[] = { 7, 8, 5, 9, 3, 4, 96 };
         int n = array.length;
-
+        Arrays.sort(array);
         int result = ob.binarySearch(array, 3, 0, n - 1);
         if (result == -1)
             System.out.println("Not found");
         else
             System.out.println("Element found at index " + result);
-
+       System.out.println("Sorted Array");
+            for(int i=0;i<array.length;i++)
+        {
+            System.out.println(array[i]);
+        }
+        
     }
 }
